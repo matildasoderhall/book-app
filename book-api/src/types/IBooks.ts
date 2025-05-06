aed9c273f4993e89e094965a8d6d026013d6e48d
Carl4000 <cmwellman@gmail.com>
2025-05-06 20:52:50 +0200

    (fix): get server to work

--- book-api/src/types/IBooks.ts
+++ book-api/src/types/IBooks.ts
@@ -1,1 +1,12 @@
 
+export interface IBooks {
+    
+    title:String
+    description:String
+    author:String
+    genres:Array
+    image:String
+    published_year:Number
+    reviews:[{type:mongoose.Schema.Types.ObjectId,ref:"Reviews"}]
+
+}
