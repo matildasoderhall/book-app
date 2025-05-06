ba81aab76a92e7402e968729034adea44fabdcb7
matildasoderhall <soderhall.matilda@gmail.com>
2025-05-06 16:07:54 +0200

    :sparkles: add fetch single user and update single user

--- book-api/src/routes/users.ts
+++ book-api/src/routes/users.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { fetchAllUsers } from "../controllers/usersController";
+import { fetchAllUsers, fetchUser, updateUser } from "../controllers/usersController";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', fetchAllUsers)
+router.get('/', fetchAllUsers);
+router.get('/:id', fetchUser);
+router.patch('/:id', updateUser);
 
 export default router;
