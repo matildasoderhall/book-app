b8195f5addc8da2de5a870bd548673a9ac256fd5
matildasoderhall <soderhall.matilda@gmail.com>
2025-05-07 12:39:05 +0200

    ✨add routes for auth

--- book-api/src/routes/auth.ts
+++ book-api/src/routes/auth.ts
@@ -1,1 +1,11 @@
+import express from 'express';
+import { login, logout, register } from '../controllers/authController';
 
+const router = express.Router();
+
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/register', register);
+
+
+export default router;
