829409b756ffdc49c7b503513c698e5966c4bfcc
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2025-05-07 10:36:33 +0200

    ✨ feat: add fetchAll-endpoint & router setup

--- book-api/src/controllers/reviewsController.ts
+++ book-api/src/controllers/reviewsController.ts
@@ -1,1 +1,12 @@
+import { Request, Response } from "express";
+import Review from '../models/Reviews';
+import Reviews from "../models/Reviews";
 
+export const fetchAllReviews = async (_: Request, res: Response) => {
+    try {
+      res.json(await Reviews.find());
+    } catch(error: unknown) {
+      const message = error  instanceof Error ? error.message : 'Unknown error'
+      res.status(500).json({error: message})
+    }
+  }
--- book-api/src/routes/reviews.ts
+++ book-api/src/routes/reviews.ts
@@ -1,1 +1,22 @@
+import express from 'express';
+import { 
+//   createReview, 
+//   deleteReview, 
+  fetchAllReviews, 
+//   fetchReview, 
+//   updateReview 
+} 
+  from "../controllers/reviewsController"
 
+// import { verifyAccessToken } from '../middleware/verifyToken';
+const router = express.Router()
+
+router.get('/', fetchAllReviews)
+// router.get('/:id', fetchReview)
+
+// The 3 endpoints below are protected
+// router.post('/', verifyAccessToken, createReview)
+// router.patch('/:id',verifyAccessToken, updateReview)
+// router.delete('/:id',verifyAccessToken, deleteReview)
+
+export default router;
