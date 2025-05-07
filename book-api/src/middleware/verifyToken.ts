515a438068515e58f2d84eaa936a9eae47c0c683
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2025-05-07 13:36:00 +0200

    ♻️ refactor: correct spelling for verifyAccessToken

--- book-api/src/middleware/verifyToken.ts
+++ book-api/src/middleware/verifyToken.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const verifyAccesToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyAccessToken = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.cookies.accessToken);
 
     if (!req.cookies.accessToken === undefined) {
--- book-api/src/routes/users.ts
+++ book-api/src/routes/users.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { deleteUser, fetchAllUsers, fetchUser, updateUser } from "../controllers/usersController";
-import { verifyAccesToken } from "../middleware/verifyToken";
+import { verifyAccessToken } from "../middleware/verifyToken";
 
 const router = express.Router();
 
 router.get('/', fetchAllUsers);
 router.get('/:id', fetchUser);
-router.patch('/:id', verifyAccesToken, updateUser);
-router.delete('/:id', verifyAccesToken, deleteUser);
+router.patch('/:id', verifyAccessToken, updateUser);
+router.delete('/:id', verifyAccessToken, deleteUser);
 
 export default router;
