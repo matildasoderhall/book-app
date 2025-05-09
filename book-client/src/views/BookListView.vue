7e6f3b1ae53b9efd9e3c72dee2a648bc8f877c4f
Carl4000 <cmwellman@gmail.com>
2025-05-09 22:20:39 +0200

    :sparkles: (feat): Booklistview core setup. followed instructions. no styling.

--- book-client/src/views/BookListView.vue
+++ book-client/src/views/BookListView.vue
@@ -1,7 +1,56 @@
 <script setup lang="ts">
   import MainHeader from "@/fixtures/MainHeader.vue";
+  import { onMounted, ref } from "vue";
+  import { RouterLink } from "vue-router";
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const books = ref([]);
+  const searchQuery = ref('');
+
+  onMounted(async () => {
+    try {
+      const response = await fetch(API_URL + 'books');
+      const data = await response.json();
+      console.log(data)
+      books.value = data;
+
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  const searchBooks = () => {
+    alert('Query:' + searchQuery.value);
+  }
+
 </script>
 
+
+
 <template>
-  <MainHeader title="Books"/>
+  <div>
+    <MainHeader title="Book List"/>
+
+    <form id="" @submit.prevent="searchBooks">
+      <input type="text" placeholder="Search" v-model="searchQuery">
+      <button>Submit</button>
+    </form>
+
+    <section id="books" v-for="book in books" :key="book.id">
+      <article>
+        <div>
+           <h3> {{ book.title }} by {{ book.author }} </h3>
+           <p> Published {{ book.published_year }}. <br>
+            Genres: {{ book.genres[0] }}, {{ book.genres[1] }} </p>
+        </div>
+
+        <div class="book-link">
+          <RouterLink :to="`/books/${book._id}`">View book</RouterLink>
+        </div>
+
+      </article>
+    </section>
+
+
+  </div>
 </template>
