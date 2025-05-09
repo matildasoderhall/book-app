57634fe42224b0e534ec8cef4b5fddd7dd8653cd
matildasoderhall <soderhall.matilda@gmail.com>
2025-05-09 14:38:48 +0200

    :lipstick: add basic layout and styling for login

--- book-client/src/views/LoginView.vue
+++ book-client/src/views/LoginView.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
   import MainHeader from "@/fixtures/MainHeader.vue";
+  import InputField from "@/components/atoms/InputField.vue";
+  import PrimaryButton from '@/components/atoms/PrimaryButton.vue';
 </script>
 
 <template>
   <MainHeader title="Login"/>
+  <form>
+    <div class="login-container">
+      <InputField type="text" placeholder="Username"/>
+      <InputField type="text" placeholder="Password"/>
+    </div>
+    <PrimaryButton type="submit" buttonLabel="Login"/>
+  </form>
 </template>
+
+<style lang="scss" scoped>
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+}
+.login-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
