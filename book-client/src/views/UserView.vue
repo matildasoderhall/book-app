fdac5ef53cc7810de2d1f4caf8114596e5c58812
matildasoderhall <soderhall.matilda@gmail.com>
2025-05-11 18:20:30 +0200

    :sparkles: add table to view and import data

--- book-client/src/views/UserView.vue
+++ book-client/src/views/UserView.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue'
   import MainHeader from "@/fixtures/MainHeader.vue";
+  import DataTable from "@/components/DataTable.vue";
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const columns = ['Username', 'Password', 'is_admin', 'created_at'];
+  const rows = ref([]);
+
+  onMounted(async () => {
+  try {
+    const res = await fetch(API_URL + 'users')
+    const data = await res.json()
+    rows.value = data;
+  } catch (error) {
+    console.error('Error fetching users:', error)
+  }
+})
 </script>
 
 <template>
   <MainHeader title="Admin Users"/>
+  <DataTable
+  :columns="columns"
+  :rows="rows"
+  />
 </template>
